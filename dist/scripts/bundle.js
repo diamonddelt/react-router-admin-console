a4d69dc94ab2980f70c459de1dd6c6c6b56c20ea
diamonddelt <rrrasti@yahoo.com>
2016-03-10 08:16:27 -0600

    added NotFoundRoute using the react-router to handle bad URLs

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -47299,8 +47299,8 @@ var Home = React.createClass({displayName: "Home",
   render: function() {
     return (
         React.createElement("div", {className: "jumbotron"}, 
-          React.createElement("h1", null, "Administration Homepage"), 
-          React.createElement("p", null, "A React/React-Router/Flux based administration homepage to manage things."), 
+          React.createElement("h1", null, "Author Admin Portal"), 
+          React.createElement("p", null, "A React/React-Router/Flux based admin portal to manage authors."), 
           React.createElement("p", null, React.createElement("h3", null, "Use the options below to get started.")), 
           React.createElement(Link, {to: "about", className: "btn btn-primary btn-large"}, "Learn more")
         )
@@ -47314,6 +47314,25 @@ module.exports = Home;
 "use strict";
 
 var React = require('react');
+var Link = require('react-router').Link;
+
+var NotFoundPage = React.createClass({displayName: "NotFoundPage",
+  render: function() {
+      return (
+        React.createElement("div", null, 
+          React.createElement("h1", null, "Page Not Found"), 
+          React.createElement("p", null, "Whoops! Sorry, there is nothing to see here!"), 
+          React.createElement("p", null, React.createElement(Link, {to: "app"}, "Back to Home"))
+        )
+      );
+  }
+});
+
+module.exports = NotFoundPage;
+},{"react":197,"react-router":28}],207:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
@@ -47322,7 +47341,7 @@ var routes = require('./routes');
 Router.run(routes, function(Handler) {
   React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
-},{"./routes":207,"react":197,"react-router":28}],207:[function(require,module,exports){
+},{"./routes":208,"react":197,"react-router":28}],208:[function(require,module,exports){
 "use strict";
 
 // React Router for handling app routing
@@ -47330,15 +47349,17 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
 
-// define initial routes
+// define initial routes which nest underneath the main 'app' route
 var routes = (
   React.createElement(Route, {name: "app", path: "/", handler: require('./components/app.js')}, 
     React.createElement(DefaultRoute, {handler: require('./components/homePage')}), 
     React.createElement(Route, {name: "authors", handler: require('./components/authors/authorPage')}), 
-    React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')})
+    React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')}), 
+    React.createElement(NotFoundRoute, {handler: require('./components/notFoundPage')})
   )
 );
 
 module.exports = routes;
-},{"./components/about/aboutPage":200,"./components/app.js":201,"./components/authors/authorPage":203,"./components/homePage":205,"react":197,"react-router":28}]},{},[206]);
+},{"./components/about/aboutPage":200,"./components/app.js":201,"./components/authors/authorPage":203,"./components/homePage":205,"./components/notFoundPage":206,"react":197,"react-router":28}]},{},[207]);
