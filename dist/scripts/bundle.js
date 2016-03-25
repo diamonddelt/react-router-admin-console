1a13ac35adf2af75c085ff0d793aa1a93c356227
diamonddelt <rrrasti@yahoo.com>
2016-03-25 06:46:25 -0500

    declared a PropTypes property within the authorForm to specify which proptypes are needed to be inherited for the child component to function

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -47564,6 +47564,17 @@ var React = require('react');
 var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({displayName: "AuthorForm",
+
+  // defining PropType values for this reusable component
+  // so that any other component using it will know which propTypes
+  // are required for successful implementation
+  propTypes: {
+    author: React.PropTypes.object.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    errors: React.PropTypes.object
+  },
+
   render: function() {
     return (
       React.createElement("div", null, 
